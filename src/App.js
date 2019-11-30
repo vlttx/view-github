f7c5d1ce57359531042f88861d016d6d0613fd49
VBTX <jv.svedaite@gmail.com>
2019-11-30 17:11:45 -0600

    add state here and use axios to fetch users

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar title={"Github viewer"} />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
