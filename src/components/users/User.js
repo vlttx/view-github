427277b888ea1371c51f036bf4e301563cdc93e8
VBTX <jv.svedaite@gmail.com>
2019-11-30 17:13:05 -0600

    add axios

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const User = props => {
   const { login, avatar_url, html_url } = props.user;
   return <div className="card text-center">User</div>;
+};
+
+User.propTypes = {
+  user: PropTypes.object.isRequired
 };
 
 export default User;
