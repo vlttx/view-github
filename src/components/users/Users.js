d7e3806b8af80a7b3d21395813df337cb39e9ba0
VBTX <jv.svedaite@gmail.com>
2019-11-30 17:23:37 -0600

    make users a functional component and add loading, spinner and proptypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import User from "./User";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-export default class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <User key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1rem"
 };
+export default Users;
