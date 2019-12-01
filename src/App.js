ea18661aea50f5d16241f9254df5cbeb5aa03a37
VBTX <jv.svedaite@gmail.com>
2019-11-30 19:10:20 -0600

    fix search request

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
     const res = await axios.get(
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+
     this.setState({ users: res.data, loading: false });
   }
 
@@ -28,8 +29,9 @@ class App extends React.Component {
   searchUsers = async text => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+    console.log(text);
     this.setState({ users: res.data.items, loading: false });
   };
 
