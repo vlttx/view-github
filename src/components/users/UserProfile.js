65476a9c4fe8ca69cea79e38b1c5f8dc86c85827
VBTX <jv.svedaite@gmail.com>
2019-11-30 18:45:28 -0600

    add UserProfile component for displaying a single user

--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
 export default class UserProfile extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params);
+  }
   render() {
-    return <div>User</div>;
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable
+    } = this.props.user;
+
+    const { loading } = this.props;
+    return <div>{name}</div>;
   }
 }
