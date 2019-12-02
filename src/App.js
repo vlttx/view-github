fcb1bf5a287b2d8ab15d50b366fe2c986abfcbbc
VBTX <jv.svedaite@gmail.com>
2019-12-01 22:10:15 -0600

    move search users from App to GithubState

--- src/App.js
+++ src/App.js
@@ -34,17 +34,6 @@ const App = () => {
     []
   );
 
-  //search github users
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   //get a single github user
 
   const getUser = async username => {
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,6 +20,23 @@ const GithubState = props => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  //search github users
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data
+    });
+  };
+
+  const setLoading = () => {
+    dispatch({ type: SET_LOADING });
+  };
+
   return (
     <GithubContext.Provider
       value={{
