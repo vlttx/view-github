b14773abeff2ff7b560db99ea5c40af06bcaf832
VBTX <jv.svedaite@gmail.com>
2019-12-02 22:39:46 -0600

    fix typos

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
-import AlertContext from "./githubContext";
-import AlertReducer from "./githubReducer";
+import AlertContext from "./alertContext";
+import AlertReducer from "./alertReducer";
 import { SET_ALERT, REMOVE_ALERT } from "../types";
 
 const AlertState = props => {
